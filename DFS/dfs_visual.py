3a48b7eb920e743849a5f60efab7f09c406fc8f8
shettyabhi48 <abhishreeshetty48@gmail.com>
2017-06-15 18:15:24 +0530

    refactoring the code

--- DFS/dfs_visual.py
+++ DFS/dfs_visual.py
@@ -2,76 +2,77 @@ import networkx as nx
 import matplotlib.pyplot as plt
  
 
+
 #utility fucntion used by DFS which does recursive depth first search 
-def DFSUtil(G, v, visited,sl): 
-	visited[v]= True
+def DFSUtil(G, v, visited, sl): 
+	visited[v] = True
 	sl.append(v) 
 	for i in G[v]:
 		if visited[i] == False:
-			DFSUtil(G, i, visited,sl)
+			DFSUtil(G, i, visited, sl)
 	return sl
  
 
+
 #DFS traversal 
-def DFS(G,source): 
-	visited =[False]*(len(G.nodes()))
- 	sl=[]		#a list that stores dfs forest starting with source node
- 	dfs_stk=[] #A nested list that stores all the DFS Forest's
-	dfs_stk.append(DFSUtil(G,source, visited,sl))
+def DFS(G, source): 
+	visited = [False]*(len(G.nodes()))
+ 	sl = []		#a list that stores dfs forest starting with source node
+ 	dfs_stk = [] #A nested list that stores all the DFS Forest's
+	dfs_stk.append(DFSUtil(G, source, visited, sl))
 	for i in range(len(G.nodes())):
 		if visited[i] == False:
-			sl=[]
-			dfs_stk.append(DFSUtil(G, i, visited,sl))
+			sl = []
+			dfs_stk.append(DFSUtil(G, i, visited, sl))
 	return dfs_stk
-
 			
 
 
 #takes input from the file and creates a weighted graph
 def CreateGraph(G):
-	f=open('input.txt')
-	n=int(f.readline())
-	wtMatrix=[]
+	f = open('input.txt')
+	n = int(f.readline())
+	wtMatrix = []
 	for i in range(n):
-		list1=map(int,(f.readline()).split())
+		list1 = map(int,(f.readline()).split())
 		wtMatrix.append(list1)
-	source=int(f.readline()) #source vertex from where DFS has to start
+	source = int(f.readline()) #source vertex from where DFS has to start
 	#Adds egdes along with their weights to the graph 
-	for i in range(n) :
-		for j in range(n) :
-			if wtMatrix[i][j]>0 :
-					G.add_edge(i,j,length=wtMatrix[i][j]) 
+	for i in range(n):
+		for j in range(n):
+			if wtMatrix[i][j] > 0:
+					G.add_edge(i, j, length = wtMatrix[i][j]) 
 	return G,source
 
 
 
-
 #marks all edges traversed through DFS with red
-def DrawDFSPath(G,dfs_stk):
+def DrawDFSPath(G, dfs_stk):
 	pos = nx.spring_layout(G)
-	nx.draw(G,pos,with_labels=True)  #with_labels=true is to show the node number in the output graph
-	edge_labels=dict([((u,v,),d['length'])
-             for u,v,d in G.edges(data=True)])
-	nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3, font_size=11) #prints weight on all the edges
-
-
+	nx.draw(G, pos, with_labels = True)  #with_labels=true is to show the node number in the output graph
+	edge_labels = dict([((u,v,), d['length']) for u, v, d in G.edges(data = True)])
+	nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, label_pos = 0.3, font_size = 11) #prints weight on all the edges
 	for i in dfs_stk:
-		if(len(i)>1):
-			for j in i[:(len(i)-1)]:
+		#if there is more than one node in the dfs-forest, then print the corresponding edges
+		if len(i) > 1:
+			for j in i[ :(len(i)-1)]:
 				if i[i.index(j)+1] in G[j]:
-					nx.draw_networkx_edges(G,pos,edgelist=[(j,i[i.index(j)+1])],width=2.5,alpha=0.6,edge_color='r')
+					nx.draw_networkx_edges(G, pos, edgelist = [(j,i[i.index(j)+1])], width = 2.5, alpha = 0.6, edge_color = 'r')
 				else:
-					for k in i[1::-1]: #if in case the path was reversed coz all the possible neighbours were visited, we need to find the adj node to it.
+					#if in case the path was reversed because all the possible neighbours were visited, we need to find the adj node to it.
+					for k in i[1::-1]: 
 						if k in G[j]:
-							nx.draw_networkx_edges(G,pos,edgelist=[(j,k)],width=2.5,alpha=0.6,edge_color='r')
+							nx.draw_networkx_edges(G, pos, edgelist = [(j,k)], width = 2.5, alpha = 0.6, edge_color = 'r')
 							break
-	plt.show()
+
 
 
 #main function
-if __name__== "__main__":
+if __name__ == "__main__":
 	G = nx.DiGraph()
-	G,source=CreateGraph(G)
-	dfs_stk=DFS(G,source)
-	DrawDFSPath(G,dfs_stk)
+	G, source = CreateGraph(G)
+	dfs_stk = DFS(G, source)
+	DrawDFSPath(G, dfs_stk)
+	plt.show()
+
 
--- Dijsktra's/dijsktras.py
+++ Dijsktra's/dijsktras.py
@@ -4,10 +4,9 @@ import sys
  
 
 
-def minDistance(dist,sptSet,V):
-	print dist
-   	min = sys.maxsize
-
+#utility function that returns the minimum distance node
+def minDistance(dist, sptSet, V):
+   	min = sys.maxsize #assigning largest numeric value to min
    	for v in range(V):
    		if sptSet[v] == False and dist[v] <= min:
    			min = dist[v]
@@ -15,72 +14,72 @@ def minDistance(dist,sptSet,V):
 	return min_index
 
 
-def dijkstra(G,source,pos):
 
-	V=len(G.nodes())
-	dist=[]
-	parent=[None]*V
-	sptSet=[] # sptSet[i] will true if vertex i is included in shortest
-               # path tree or shortest distance from src to i is finalized
-
+#function that performs dijsktras algorithm on the graph G,with source vertex as source
+def dijsktras(G, source, pos):
+	V = len(G.nodes()) # V denotes the number of vertices in G
+	dist = [] # dist[i] will hold the shortest distance from source to i
+	parent = [None]*V # parent[i] will hold the node from which i is reached to, in the shortest path from source
+	sptSet = [] # sptSet[i] will true if vertex i is included in shortest path tree
+	#initially, for every node, dist[] is set to maximum value and sptSet[] is set to False
 	for i in range(V):
 		dist.append(sys.maxsize)
 		sptSet.append(False)
 
 	dist[source] = 0
-	parent[source]=-1
+	parent[source]=-1 #source is itself the root, and hence has no parent
 
 	for count in range(V-1):
-		u = minDistance(dist, sptSet,V)
+		u = minDistance(dist, sptSet, V) #pick the minimum distance vectex from the set of vertices
 		sptSet[u] = True
+		#update the vertices adjacent to the picked vertex
 		for v in range(V):
-			print sptSet[u]
-			if (u,v) in G.edges():
-				if sptSet[v]==False and dist[u] != sys.maxsize and dist[u]+G[u][v]['length'] < dist[v]:
+			if (u, v) in G.edges():
+				if sptSet[v] == False and dist[u] != sys.maxsize and dist[u] + G[u][v]['length'] < dist[v]:
 					dist[v] = dist[u] + G[u][v]['length']
 					parent[v] = u
+	#marking the shortest path from source to each of the vertex with red, using parent[]
 	for X in range(V):
-		if parent[X]!=-1:
-			if (parent[X],X) in G.edges():
-				nx.draw_networkx_edges(G,pos,edgelist=[(parent[X],X)],width=2.5,alpha=0.6,edge_color='r')
+		if parent[X] != -1: #ignore the parent of root node 
+			if (parent[X], X) in G.edges():
+				nx.draw_networkx_edges(G, pos, edgelist = [(parent[X], X)], width = 2.5, alpha = 0.6, edge_color = 'r')
 	return
 
 
 
 #takes input from the file and creates a weighted graph
 def CreateGraph(G):
-	f=open('input.txt')
-	n=int(f.readline())
-	wtMatrix=[]
+	f = open('input.txt')
+	n = int(f.readline())
+	wtMatrix = []
 	for i in range(n):
-		list1=map(int,(f.readline()).split())
+		list1 = map(int, (f.readline()).split())
 		wtMatrix.append(list1)
-	source=int(f.readline()) #source vertex for dijsktra's algo 
+	source = int(f.readline()) #source vertex for dijsktra's algo 
 	#Adds egdes along with their weights to the graph 
 	for i in range(n) :
 		for j in range(n) :
-			if wtMatrix[i][j]>0 :
-					G.add_edge(i,j,length=wtMatrix[i][j]) 
-	return G,source
-
+			if wtMatrix[i][j] > 0 :
+					G.add_edge(i, j, length = wtMatrix[i][j]) 
+	return G, source
 
 
 
 #draws the graph and displays the weights on the edges
 def DrawGraph(G):
 	pos = nx.spring_layout(G)
-	nx.draw(G,pos,with_labels=True)  #with_labels=true is to show the node number in the output graph
-	edge_labels=dict([((u,v,),d['length'])
-             for u,v,d in G.edges(data=True)])
-	nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3, font_size=11) #prints weight on all the edges
+	nx.draw(G, pos, with_labels = True)  #with_labels=true is to show the node number in the output graph
+	edge_labels = dict([((u, v), d['length']) for u, v, d in G.edges(data = True)])
+	nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, label_pos = 0.3, font_size = 11) #prints weight on all the edges
 	return pos
 
 
+
 #main function
-if __name__== "__main__":
+if __name__ == "__main__":
 	G = nx.DiGraph()
-	G,source=CreateGraph(G)
-	pos=DrawGraph(G)
-	dijkstra(G,source,pos)
+	G,source = CreateGraph(G)
+	pos = DrawGraph(G)
+	dijsktras(G, source, pos)
 	plt.show()
 
