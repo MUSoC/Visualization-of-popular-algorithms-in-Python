9eebbd9868ff1df0475e7ab91c07f4af3f7ecf45
shettyabhi48 <abhishreeshetty48@gmail.com>
2017-07-15 00:49:06 +0530

    refactoring the code

--- K Centers Problem/k_centers_problem.py
+++ K Centers Problem/k_centers_problem.py
@@ -1,26 +1,26 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import sys
 import operator
 
 
 
-def k_centers(G,n):
+def k_centers(G, n):
 	centers = []
 	cities = G.nodes()
-	#add an arbitrary node, here, the first node,to he centers list
+	#add an arbitrary node, here, the first node,to the centers list
 	centers.append((G.nodes())[0])
 	cities.remove(centers[0]) 
 	n = n-1 #since we have already added one center
-	while n!=0:
+	#choose n-1 centers
+	while n!= 0:
 		city_dict = {}
 		for cty in cities:
-			min_dist=float("inf")
+			min_dist = float("inf")
 			for c in centers:
-				min_dist=min(min_dist,G[cty][c]['length'])
-			city_dict[cty]=min_dist
+				min_dist = min(min_dist,G[cty][c]['length'])
+			city_dict[cty] = min_dist
 		#print city_dict
-		new_center = max(city_dict, key=lambda i: city_dict[i])
+		new_center = max(city_dict, key = lambda i: city_dict[i])
 		#print new_center
 		centers.append(new_center)
 		cities.remove(new_center)
@@ -29,11 +29,12 @@ def k_centers(G,n):
 	return centers
 
 
+
 #takes input from the file and creates a weighted undirected graph
 def CreateGraph():
 	G = nx.Graph()
 	f = open('input.txt')
-	n = int(f.readline())
+	n = int(f.readline()) #n denotes the number of cities
 	wtMatrix = []
 	for i in range(n):
 		list1 = map(int, (f.readline()).split())
@@ -42,28 +43,28 @@ def CreateGraph():
 	for i in range(n) :
 		for j in range(n)[i:] :
 				G.add_edge(i, j, length = wtMatrix[i][j]) 
-	n = int(f.readline())
-	return G,n
+	noc = int(f.readline()) #noc,here,denotes the number of centers
+	return G, noc
 
 
 
 #draws the graph and displays the weights on the edges
-def DrawGraph(G,centers):
+def DrawGraph(G, centers):
 	pos = nx.spring_layout(G)
- 	color_map = ['blue']*len(G.nodes())
- 	for nod in centers:
- 		color_map[nod] = 'red'
-	nx.draw(G, pos,node_color = color_map, with_labels = True)  #with_labels=true is to show the node number in the output graph
-	edge_labels = nx.get_edge_attributes(G,'length')
+ 	color_map = ['blue'] * len(G.nodes())
+ 	#all the center nodes are marked with 'red'
+ 	for c in centers:
+ 		color_map[c] = 'red'
+	nx.draw(G, pos, node_color = color_map, with_labels = True)  #with_labels=true is to show the node number in the output graph
+	edge_labels = nx.get_edge_attributes(G, 'length')
 	nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 11) #prints weight on all the edges
 	
-
 
 
 #main function
 if __name__ == "__main__":
 	G,n = CreateGraph()
 	centers = k_centers(G, n)
-	DrawGraph(G,centers)
+	DrawGraph(G, centers)
 	plt.show()
 
