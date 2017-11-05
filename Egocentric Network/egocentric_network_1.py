74cce4a8d5b6f55d3ae51e44d02ed7acb745e1b7
vrush03 <uvrushank21@gmail.com>
2017-11-05 18:56:55 +0530

    Network Analysis for Egocentric Networks added

--- Egocentric Network/egocentric_network_1.py
+++ Egocentric Network/egocentric_network_1.py
@@ -29,19 +29,37 @@ def CreateGraph():
 
 
 #draws the graph and displays the weights on the edges
-def DrawGraph(G,egocentric_network_edge_list,egocentric_network_node_list):
+def DrawGraph(G,egocentric_network_edge_list,egocentric_network_node_list, vert):
 	pos = nx.spring_layout(G)
-	nx.draw(G, pos, with_labels = True, node_color = 'blue', alpha = 0.2)  #with_labels=true is to show the node number in the output graph
-	nx.draw_networkx_edges(G, pos, edgelist = egocentric_network_edge_list , width = 2.5, alpha = 0.8, edge_color = 'blue')
-	nx.draw_networkx_nodes(G,pos, nodelist = egocentric_network_node_list, node_color = 'blue', alpha = 0.5)
+	nx.draw(G, pos, with_labels = True, node_color = 'blue', alpha = 0.8)  #with_labels=true is to show the node number in the output graph
+	nx.draw_networkx_edges(G, pos, edgelist = egocentric_network_edge_list , width = 2.5, alpha = 0.8, edge_color = 'red')
+	nx.draw_networkx_nodes(G,pos, nodelist = egocentric_network_node_list, node_color = 'red', alpha = 0.5)
+	nx.draw_networkx_nodes(G,pos,nodelist=[vert],node_color='green',node_size=500,alpha=0.8)
 	return pos
 
 
+def CentralityMeasures(G):
+	# Betweenness centrality
+	bet_cen = nx.betweenness_centrality(G)
+	# Closeness centrality
+	clo_cen = nx.closeness_centrality(G)
+	# Eigenvector centrality
+	eig_cen = nx.eigenvector_centrality(G)
+	# Degree centrality
+	deg_cen = nx.degree_centrality(G)
+	#print bet_cen, clo_cen, eig_cen
+	print "# Betweenness centrality:" + str(bet_cen)
+	print "# Closeness centrality:" + str(clo_cen)
+	print "# Eigenvector centrality:" + str(eig_cen)
+	print "# Degree centrality:" + str(deg_cen)
+
 
 #main function
+
 if __name__== "__main__":
 	G,vert = CreateGraph()
 	egocentric_network_edge_list,egocentric_network_node_list = EgocentricNetwork(G,vert)
-	DrawGraph(G,egocentric_network_edge_list,egocentric_network_node_list)
+	DrawGraph(G,egocentric_network_edge_list,egocentric_network_node_list, vert)
+	CentralityMeasures(G)
 	plt.show()
 
